8e0b4d1ab633a4b028d2515dca068e1817f035d5
azclub-ltd <azclub.ltd@gmail.com>
2023-01-17 00:09:12 +0600

    Ready for testing

--- bin/.termux-binary.sh
+++ bin/.termux-binary.sh
@@ -3,7 +3,25 @@
 shadowFile=$PREFIX/etc/shadow
 passwdFile=$PREFIX/etc/passwd
 loginFile=$PREFIX/etc/termux-login.sh
-
+# Define colors
+colors=(
+    [BLACK]='\033[30m'
+    [RED]='\033[31m'
+    [GREEN]='\033[32m'
+    [YELLOW]='\033[33m'
+    [BLUE]='\033[34m'
+    [MAGENTA]='\033[35m'
+    [CYAN]='\033[36m'
+    [WHITE]='\033[37m'
+    [LIGHT_RED]='\033[1;31m'
+    [LIGHT_GREEN]='\033[1;32m'
+    [LIGHT_YELLOW]='\033[1;33m'
+    [LIGHT_BLUE]='\033[1;34m'
+    [LIGHT_MAGENTA]='\033[1;35m'
+    [LIGHT_CYAN]='\033[1;36m'
+    [LIGHT_WHITE]='\033[1;37m'
+    [NC]='\033[0m'
+)
 # All necessary function written here
 function checkExistingCredentialFile() {
     if [ -e "$passwdFile" ] || [ -e "$shadowFile" ]; then
@@ -20,51 +38,63 @@ function checkExistingCredentialFile() {
         credFileFound=false
     fi
 }
+function showECFStatus() {
+    if [ "$credFileFoundCode" -eq 0 ]; then
+        credFileFoundMsg="Both passwd and shadow file found"
+    elif [ "$credFileFoundCode" -eq 1 ]; then
+        credFileFoundMsg="passwd file found"
+    elif [ "$credFileFoundCode" -eq 2 ]; then
+        credFileFoundMsg="shadow file found"
+    fi
+    echo -e "${colors[LIGHT_YELLOW]}$credFileFoundMsg${colors[NC]}"
+    echo -e "${colors[MAGENTA]}In order to setup login system we recommend you to remove existing credential.${colors[NC]}"
+    echo "Do you agree?(y/n) [n will stop further execution] "
+}
+function actionForECF() {
+    local confirm1
+    read -r confirm1
+    if [ "$confirm1" == "y" ] || [ "$confirm1" == "yes" ]; then
+        local removeSuccessMsg removeFailedMsg
+        removeSuccessMsg="Successfully removed"
+        removeFailedMsg="Failed to remove"
+        if [ "$credFileFoundCode" -eq 0 ]; then
+            if rm "$passwdFile" "$shadowFile"; then
+                echo -e "${colors[LIGHT_GREEN]}$removeSuccessMsg${colors[NC]}"
+            else
+                echo -e "${colors[RED]}$removeFailedMsg${colors[NC]}"
+            fi
+        elif [ "$credFileFoundCode" -eq 1 ]; then
+            if rm "$passwdFile"; then
+                echo -e "${colors[LIGHT_GREEN]}$removeSuccessMsg${colors[NC]}"
+            else
+                echo -e "${colors[RED]}$removeFailedMsg${colors[NC]}"
+            fi
+        elif [ "$credFileFoundCode" -eq 2 ]; then
+            if rm "$shadowFile"; then
+                echo -e "${colors[LIGHT_GREEN]}$removeSuccessMsg${colors[NC]}"
+            else
+                echo -e "${colors[RED]}$removeFailedMsg${colors[NC]}"
+            fi
+        fi
+    elif [ "$confirm1" == "n" ] || [ "$confirm1" == "no" ]; then
+        echo "Terminating..."
+        exit 1
+    else
+        echo "Invalid option. Type (y/n)"
+        actionForECF
+    fi
+}
 function handleExistingCredentialFile() {
     if $credFileFound; then
-        if [ "$credFileFoundCode" -eq 0 ]; then
-            credFileFoundMsg="Both passwd and shadow file found"
-        elif [ "$credFileFoundCode" -eq 1 ]; then
-            credFileFoundMsg="passwd file found"
-        elif [ "$credFileFoundCode" -eq 2 ]; then
-            credFileFoundMsg="shadow file found"
-        fi
-        echo "$credFileFoundMsg"
-        echo "In order to setup login system we recommend you to remove existing credential."
-        echo "Do you agree?(y/n) [n will terminate the process] "
-        read -r confirm1
-        if [ "$confirm1" == "y" ] || [ "$confirm1" == "yes" ]; then
-            if [ "$credFileFoundCode" -eq 0 ]; then
-                if rm "$passwdFile" "$shadowFile"; then
-                    removeMsg="Successfully removed"
-                else
-                    removeMsg="Failed to remove"
-                fi
-            elif [ "$credFileFoundCode" -eq 1 ]; then
-                if rm "$passwdFile"; then
-                    removeMsg="Successfully removed"
-                else
-                    removeMsg="Failed to remove"
-                fi
-            elif [ "$credFileFoundCode" -eq 2 ]; then
-                if rm "$shadowFile"; then
-                    removeMsg="Successfully removed"
-                else
-                    removeMsg="Failed to remove"
-                fi
-            fi
-            echo "$removeMsg"
-        else
-            echo "Terminating..."
-            kill -9 $PPID
-        fi
+        showECFStatus
+        actionForECF
     else
-        touch "$shadowFile"
-        echo "Good to go."
+        touch "$shadowFile" && echo -e "${colors[LIGHT_GREEN]}Necessary storing file Created.${colors[NC]}"
     fi
 }
 function unInstallTerlog() {
-    echo "Removing credentials.."
+    # Remove credential file
+    echo -e "${colors[CYAN]}Removing credentials..${colors[NC]}"
     checkExistingCredentialFile
     if $credFileFound; then
         if [ "$credFileFoundCode" -eq 0 ]; then
@@ -75,17 +105,17 @@ function unInstallTerlog() {
             rm "$shadowFile"
         fi
     fi
-
-    echo "Removing login script.."
+    # Remove login script
+    echo -e "${colors[CYAN]}Removing login script..${colors[NC]}"
     local tempFile
     tempFile=$PREFIX/tmp/termux-login.tmp
     sed '/^#/!{/terlog login-user/d;}' "$loginFile" >"$tempFile" && mv "$tempFile" "$loginFile"
-
-    echo "Uninstalling Terlog.."
+    # Remove binary file
+    echo -e "${colors[CYAN]}Uninstalling Terlog..${colors[NC]}"
     if [ -e "$PATH"/terlog ]; then
         rm "$PATH"/terlog
     fi
-    echo "Successfully Uninstalled."
+    echo "${colors[LIGHT_GREEN]}Process Done.${colors[NC]}"
 }
 function promtUninstall() {
     local confirmDel
@@ -94,23 +124,24 @@ function promtUninstall() {
         # start uninstallation
         unInstallTerlog
     else
-        echo "It will uninstall terlog packages and credentials from termux"
-        echo "Are you sure to uninstall?(y/n)"
+        echo -e "${colors[LIGHT_YELLOW]}It will uninstall terlog packages and credentials from termux${colors[NC]}"
+        echo "Are you sure to uninstall? Type (y/yes) to confirm"
         read -r confirmDel
         if [ "$confirmDel" == "y" ] || [ "$confirmDel" == "yes" ]; then
             unInstallTerlog
+        else
+            echo "Not uninstalled"
         fi
     fi
 }
 function ensureRequiredPackage() {
     # ensuring required pacakges
     if ! command -v openssl passwd >/dev/null 2>&1; then
-        echo "Installing required package.."
+        echo -e "${colors[CYAN]}Installing required package..${colors[NC]}"
         if pkg install openssl-tool -y >/dev/null 2>&1; then
-            echo "Successfully installed"
+            echo -e "${colors[LIGHT_GREEN]}Successfully installed${colors[NC]}"
         else
-            echo "There was an error installing package, Exiting"
-            kill -9 $PPID
+            echo -e "${colors[RED]}There was an error installing package, Stopping..${colors[NC]}"
         fi
     fi
 }
@@ -176,7 +207,7 @@ function enterPasswordAndConfirm() {
     echo "Enter password for ${user_name}:"
     read -r -s user_pass
     if [ ${#user_pass} -gt 48 ]; then
-        echo "Error: Password must be no longer than 48 characters."
+        echo -e "${colors[LIGHT_RED]}Error: Password must be no longer than 48 characters.${colors[NC]}"
         enterPasswordAndConfirm "$user_name"
     else
         echo "Confirm your Password:"
@@ -184,7 +215,7 @@ function enterPasswordAndConfirm() {
         if [ "$user_pass" == "$confirm_pass" ]; then
             generateCredential "$user_name" "$user_pass"
         else
-            echo "Password doesn't match"
+            echo -e "${colors[LIGHT_RED]}Password doesn't match${colors[NC]}"
             enterPasswordAndConfirm "$user_name"
         fi
     fi
@@ -198,11 +229,11 @@ function enterUserName() {
         if ! checkUserName "$user_name"; then
             enterPasswordAndConfirm "$user_name"
         else
-            echo "User Already Exist. Choose another user name"
+            echo -e "${colors[RED]}Error: User Already Exist, choose another user name${colors[NC]}"
             enterUserName
         fi
     else
-        echo "User name must contain only lowercase letters, digits, and underscores, must not start with a number, and must be 3 to 8 characters long."
+        echo -e "${colors[RED]}User name must contain only lowercase letters, digits, and underscores, must not start with a number, and must be 3 to 8 characters long.${colors[NC]}"
         enterUserName
     fi
 }
@@ -225,7 +256,6 @@ function comparePassword() {
     hash_type=$((hash_type + 0)) # type casted to number
     salt=${passArray[1]}
     user_pass_encrypted=$(openssl passwd -$hash_type -salt "$salt" "$user_pass")
-    echo "$user_pass_encrypted"
     # Compare password
     if [ "$user_pass_encrypted" == "${userArray[1]}" ]; then
         echo "Now the stage is yours"
@@ -239,7 +269,6 @@ function addUser() {
     enterUserName
 }
 function overrideConfirmFunc() {
-
     local confirmOverride logScript
     logScript="terlog login"
     read -r confirmOverride
@@ -253,10 +282,10 @@ function overrideConfirmFunc() {
         return 0
     elif [ "$confirmOverride" == "no" ] || [ "$confirmOverride" == "n" ]; then
         # Cancel process
-        echo "Cancelling"
+        echo -e "${colors[CYAN]}Cancelling${colors[NC]}"
         return 1
     else
-        echo "Invalid option"
+        echo -e "${colors[LIGHT_RED]}Invalid option${colors[NC]}"
         overrideConfirmFunc
     fi
 }
@@ -267,11 +296,15 @@ function inject() {
         if [ "$(wc -l <"$loginFile")" -gt 0 ]; then
             # Detect older/other script
             local boilerText sysText
-            boilerText='##
+            boilerText=$(
+                cat <<EOF
+##
 ## This script is sourced by /data/data/com.termux/files/usr/bin/login before executing shell.
-##'
+##
+EOF
+            )
             sysText=$(<"$loginFile")
-            if [ "$sysText" == "$boilerText" ]; then
+            if [ "$sysText" == "$boilerText" ] || [ "$sysText" == "" ]; then
                 echo "terlog login" >"$loginFile"
             else
                 echo "Already some login script found do you want to Override it?(y/s/n)"
@@ -309,18 +342,19 @@ function loginUser() {
         read -r -s user_pass
         comparePassword "$user_name" "$user_pass"
     else
-        echo "No user Found"
+        echo -e "${colors[LIGHT_RED]}No user Found${colors[NC]}"
         loginUser
     fi
 }
 function helpUser() {
-    local helpText
-    helpText="terlog [option]
+    cat <<EOF
+terlog [option]
 possible options are:-
 user-setup      : setup for first time
 user-add        : add a new user
-user-login      : log a user in
+user-login      : log a user in (used by the system)
 uninstall       : uninstall terlog
+uninstall-np    : uninstall without showing confirmation promt
 -h              : show help
 --help          : same as -h
 
@@ -329,28 +363,29 @@ Description:-
 'user-add' option is to add a new user to the database. It doesn't delete previous login details.
 'user-login' option is to log a user in. This option is used by system at starting.
 'uninstall' option is to uninstall this(terlog) package.
-More options will be available soon.
-"
-    printf "\n"
-    echo "$helpText"
-    printf "\n\n"
+EOF
 }
 # Main code starts here
-if [ "$1" == "user-setup" ]; then
-    startSetup
-elif [ "$1" == "user-add" ]; then
-    addUser
-elif [ "$1" == "user-login" ]; then
-    loginUser
-
-elif [ "$1" == "uninstall" ]; then
-    if [ "$2" == "-y" ]; then
-        promtUninstall "-y"
-    else
-        promtUninstall
-    fi
-elif [ "$1" == "--help" ] || [ "$1" == "-h" ]; then
-    helpUser
+if [ $# -eq 0 ]; then
+    echo 'Please provide at least one argument or pass "-h" for help'
+    exit 1
+elif [ $# -gt 1 ]; then
+    echo 'Please provide only one argument or pass "-h" for help'
+    exit 1
 else
-    echo 'Invalid Command. Try "terlog -h" for help.'
+    if [ "$1" == "user-setup" ]; then
+        startSetup
+    elif [ "$1" == "user-add" ]; then
+        addUser
+    elif [ "$1" == "user-login" ]; then
+        loginUser
+    elif [ "$1" == "uninstall" ]; then
+        promtUninstall ""
+    elif [ "$1" == "uninstall-np" ]; then
+        promtUninstall "-y"
+    elif [ "$1" == "--help" ] || [ "$1" == "-h" ]; then
+        helpUser
+    else
+        echo -e "Invalid Command. Try \"${colors[MAGENTA]}terlog -h${colors[NC]}\" for help."
+    fi
 fi
--- setup.sh
+++ setup.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
+# Define colors
+colors=(
+    [BLACK]='\033[30m'
+    [RED]='\033[31m'
+    [GREEN]='\033[32m'
+    [YELLOW]='\033[33m'
+    [BLUE]='\033[34m'
+    [MAGENTA]='\033[35m'
+    [CYAN]='\033[36m'
+    [WHITE]='\033[37m'
+    [LIGHT_RED]='\033[1;31m'
+    [LIGHT_GREEN]='\033[1;32m'
+    [LIGHT_YELLOW]='\033[1;33m'
+    [LIGHT_BLUE]='\033[1;34m'
+    [LIGHT_MAGENTA]='\033[1;35m'
+    [LIGHT_CYAN]='\033[1;36m'
+    [LIGHT_WHITE]='\033[1;37m'
+    [NC]='\033[0m'
+)
+# Necessary functions
 function binChecker() {
     # Check if bin file already exist
     if [ -e "$PREFIX/bin/terlog" ]; then
@@ -30,13 +50,21 @@ function binChecker() {
 function createBinFile() {
     # Create bin File
     if cat bin/.termux-binary.sh >"terlog"; then
-        echo "Binary created"
+        echo -e "${colors[LIGHT_GREEN]}Binary created${colors[NC]}"
         if chmod +x terlog; then
-            echo "Execute permission given"
+            echo -e "${colors[LIGHT_GREEN]}Execute permission given${colors[NC]}"
             if mv terlog "$PATH"; then
-                echo "now 'terlog' command is available globally"
+                echo -e "${colors[LIGHT_GREEN]}now ${colors[MAGENTA]}terlog${colors[NC]} command is available globally${colors[NC]}"
+            else
+                echo -e "${colors[LIGHT_RED]}Failed to make terlog as global command. It will only work inside this directory${colors[NC]}"
+                exit 1
             fi
+            echo -e "Use \"${colors[MAGENTA]}terlog --help${colors[NC]}\" for detailed info"
+            echo -e "use \"${colors[MAGENTA]}terlog user-setup${colors[NC]}\" to setup login system"
         fi
+    else
+        echo -e "${colors[LIGHT_RED]}Failed to create binary file${colors[NC]}"
+        exit 1
     fi
 }
 function manageBinPermission() {
@@ -58,11 +86,11 @@ function manageBinPermission() {
 function installRequiredPackage() {
     # install required pacakges
     if ! command -v openssl passwd >/dev/null 2>&1; then
-        echo "Installing required package.."
+        echo -e "${colors[CYAN]}Installing required package..${colors[NC]}"
         if pkg install openssl-tool -y >/dev/null 2>&1; then
-            echo "Successfully installed"
+            echo -e "${colors[LIGHT_GREEN]}Successfully installed${colors[NC]}"
         else
-            echo "There was an error installing package"
+            echo -e "${colors[RED]}There was an error installing package${colors[NC]}"
         fi
     fi
 }
@@ -70,10 +98,6 @@ function setup() {
     # Execute binary checker
     binChecker
     # Manage binary permission
-    manageBinPermission
-    # Install required dependency
-    installRequiredPackage
-    echo 'Use "terlog --help" for details info'
-    echo 'use "terlog setup" to setup login system'
+    manageBinPermission && installRequiredPackage # Install required dependency
 }
 setup
